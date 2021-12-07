801970ad918312afd666d2ae1a25ed4b343aa148
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-07 16:19:37 -0500

    feature(Header layout, MUI/core, MUI/icons): added basics for Header component and imported MUI core and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h2>Lets build gmail</h2>
+      <Header />
       {/* Header */}
+      <h2>Lets build gmail</h2>
+
       {/* Sidebar */}
     </div>
   );
