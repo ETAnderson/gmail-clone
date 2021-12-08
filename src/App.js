af41bf6c72868afbaba6a2b0fb009e24f181055d
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-08 14:53:12 -0500

    feature(Sidebar): initial architecture)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar'
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Header */}
-      <h2>Lets build gmail</h2>
-
-      {/* Sidebar */}
+      <Sidebar />
     </div>
   );
 }
