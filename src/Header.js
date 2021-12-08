b6193389322d5a48c2b700e24f4a4b83e1709ef4
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-08 14:48:21 -0500

    feature(Header): basic styling and layout complete

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import './Header.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from '@material-ui/core';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Avatar } from '@mui/material';
+
 
 function Header() {
     return (
         <div className="header">
-            <h2>This is the header</h2>
+            <div className="header__left">
+                <IconButton >
+                    <MenuIcon />
+                </IconButton>
+                <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" 
+                    alt="" />
+            </div>
+
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder="Search mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret" />
+            </div>
+
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+
             
         </div>
     )
