f1742629060cf9a85211fd06aa00065f79e6b184
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-08 16:30:02 -0500

    feature(SidebarOption): initial architecture,layout and styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,13 @@ import { Button } from '@mui/material'
 import React from 'react'
 import './Sidebar.css'
 import AddIcon from '@mui/icons-material/Add';
+import InboxIcon from '@mui/icons-material/Inbox';
+import StarIcon from '@mui/icons-material/Star';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SidebarOption from './SidebarOption';
+
+
+
 
 function Sidebar() {
     return (
@@ -12,6 +19,21 @@ function Sidebar() {
             >
                 Compose
             </Button>
+
+            <SidebarOption 
+                Icon={InboxIcon} 
+                title="Inbox" 
+                number ={54}
+                selected={true}/>
+            <SidebarOption 
+                Icon={StarIcon} 
+                title="Starred" 
+                number ={54}/>
+            <SidebarOption 
+                Icon={AccessTimeIcon} 
+                title="Snoozed" 
+                number ={54}/>
+
         </div>
     )
 }
