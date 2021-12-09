2a4ce4083a98135f1f9a387b5e121071574856aa
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-09 13:34:19 -0500

    feature(Sidebar__footer): layout and styling added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Button, IconButton } from '@mui/material'
 import React from 'react'
 import './Sidebar.css'
 import AddIcon from '@mui/icons-material/Add';
@@ -6,8 +6,13 @@ import InboxIcon from '@mui/icons-material/Inbox';
 import StarIcon from '@mui/icons-material/Star';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import SidebarOption from './SidebarOption';
-
-
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import NoteIcon from '@mui/icons-material/Note';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PersonIcon from '@mui/icons-material/Person';
+import DuoIcon from '@mui/icons-material/Duo';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 
 function Sidebar() {
@@ -24,15 +29,52 @@ function Sidebar() {
                 Icon={InboxIcon} 
                 title="Inbox" 
                 number ={54}
-                selected={true}/>
+                selected={true}
+            />
             <SidebarOption 
                 Icon={StarIcon} 
                 title="Starred" 
-                number ={54}/>
+                number ={54}
+            />
             <SidebarOption 
                 Icon={AccessTimeIcon} 
                 title="Snoozed" 
-                number ={54}/>
+                number ={54}
+            />
+            <SidebarOption 
+                Icon={LabelImportantIcon} 
+                title="Important" 
+                number ={54}
+            />
+            <SidebarOption 
+                Icon={NearMeIcon} 
+                title="Sent" 
+                number ={54}
+            />
+            <SidebarOption 
+                Icon={NoteIcon} 
+                title="Drafts" 
+                number ={54}
+            />
+            <SidebarOption 
+                Icon={ExpandMoreIcon} 
+                title="More" 
+                number ={54}
+            />
+
+            <div className="sidebar__footer">
+                <div className="sidebar__footerIcons">
+                    <IconButton>
+                        <PersonIcon />
+                    </IconButton>
+                    <IconButton>
+                        <DuoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <PhoneIcon />
+                    </IconButton>
+                </div>
+            </div>
 
         </div>
     )
