0bbce84edeffe8f203bcce865f2b6aa8597672af
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-09 13:56:17 -0500

    feature(react-router): installed react-router, started layout of app__body

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
+    <Router>
     <div className="app">
       <Header />
-      <Sidebar />
+      <div className="app__body">
+        <Sidebar />
+
+        <Switch>
+          <Route path="/mail">
+            <EmailList />
+          </Route>
+        </Switch>
+
+      </div>
     </div>
+    </Router>
   );
 }
 
