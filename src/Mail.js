716475ea30df44045a9661660b8b149c8a3ebb75
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-10 14:46:23 -0500

    feature(Mail): styling and layout complete

--- src/Mail.js
+++ src/Mail.js
@@ -59,7 +59,18 @@ function Mail({}) {
 
                 </div>
             </div>
-            <h1>hellos</h1>
+            <div className="mail__body">
+                <div className="mail__bodyHeader">
+                    <h2>Subject</h2>
+                    <LabelImportantIcon className="mail__important" />
+                    <p>Title</p>
+                    <p className="mail__time">Time</p>
+                </div>
+
+                <div className="mail__message">
+                    <p>This is a message</p>
+                </div>
+            </div>
         </div>
     )
 }
