c7a115b5424ed7667f2292f0c6f12b2a12721f35
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-13 16:41:26 -0500

    feature(redux-devTools): installed and imported and tied into chrome

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mailReducer from '../features/mail/mailSlice';
+// devToolsEnhancer is for the redux devtools extenstion in chromium
+// eslint-disable-next-line
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 
 export const store = configureStore({
   reducer: {
     mail: mailReducer,
   },
+  // eslint-disable-next-line
+  devToolsEnhancer(){}
 });
