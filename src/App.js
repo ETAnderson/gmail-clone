7618f635aeb25d19b33eca257b8bf8348db6c1f7
Eric Anderson <anderson.eric.t@gmail.com>
2021-12-16 12:05:59 -0500

    feature(login persistence): added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar'
@@ -7,14 +7,28 @@ import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
 import { selectSendMessageIsOpen } from './features/mail/mailSlice';
-import { selectUser } from './features/user/userSlice';
-import { useSelector } from 'react-redux';
+import { login, selectUser } from './features/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import Login from './Login';
+import { auth } from './firebase';
 
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL,
+        }))
+      }
+    })
+  }, [])
 
   return (
     <Router>
